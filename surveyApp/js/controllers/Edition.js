11d14b5c1868709da1bd1ecc28d31c5e63aca614
Clembl-git <clement.mab@gmail.com>
2016-06-23 21:27:54 +0200

    Redirection / routing

---
+++ surveyApp/js/controllers/Edition.js
@@ -0,0 +1,5 @@
+angular.module('controllers')
+.controller('EditionControleur', ['$rootScope','$scope','$http','$routeParams','$location', 'Get',
+function ($rootScope, $scope,  $http, $routeParams, $location, Get) {
+  console.log("EditionControleur");
+}]);
